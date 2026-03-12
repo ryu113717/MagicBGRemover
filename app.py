26fb3d0b863581f2491f35a6c58c491afa38272d
ryu113717 <ryu.nishida113@gmail.com>
2026-03-13 02:15:06 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, send_file
+from rembg import remove
+from PIL import Image
+import io
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET","POST"])
+def index():
+
+    if request.method == "POST":
+
+        file = request.files["image"]
+
+        img = Image.open(file)
+
+        output = remove(img)
+
+        img_io = io.BytesIO()
+        output.save(img_io, "PNG")
+        img_io.seek(0)
+
+        return send_file(img_io, mimetype="image/png")
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
